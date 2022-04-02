b9952f3dffdb64073613efd37d1f436fe2e3a74d
s-beat17 <s-beat@yandex.ru>
2022-04-02 11:37:51 +0300

    files

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,8 +1,21 @@
 package pack;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Start");
+        Path path = Paths.get("file.txt");
+        try {
+            Files.write(path, "ПРивет NIO".getBytes("cp1251"));
+            byte[] bytes = Files.readAllBytes(path);
+            System.out.println(new String(bytes, "cp1251"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
